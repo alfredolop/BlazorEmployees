3e7e53035ecf379bef9bd65a54024724b9dc6000
alfredolop <alfredolop710@gmail.com>
2020-06-15 17:54:57 +0200

    Creo la conexion con sql y hago un commit para llevar las tablas que tengo en EmployeeInfo al sql

--- BlazorCRUB/BlazorCRUB/Startup.cs
+++ BlazorCRUB/BlazorCRUB/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorCRUB.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorCRUB
 {
@@ -29,6 +30,8 @@ namespace BlazorCRUB
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
